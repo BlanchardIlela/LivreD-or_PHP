6717af7792d0b2033bdeccacfb5ee090ebf94223
BlanchardIlela <blancilela@gmail.com>
2026-02-17 16:27:07 +0100

    Construction formulaire

--- index.php
+++ index.php
@@ -3,5 +3,17 @@ $title = "Livre d'or";
 require 'elements/header.php';
 ?>
 
+<div class="container">
+    <h1>Livre d'or</h1>
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="text" name="username" id="" placeholder="Votre pseudo" class="form-control">
+        </div>
+        <div class="form-group">
+            <textarea name="message" id="" placeholder="Votre Message" class="form-control"></textarea>
+        </div>
+        <button class="btn btn-primary" type="submit">Envoyer</button>
+    </form>
+</div>
 
 <?php require 'elements/footer.php'; ?>
