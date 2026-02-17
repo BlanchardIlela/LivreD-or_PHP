f18fcb16b15c9344384ce6f412c06630afe438a6
BlanchardIlela <blancilela@gmail.com>
2026-02-17 16:37:36 +0100

    Construction du nouveau message en injectant le nom de l'utilisateur et message tout en vérifiant que il n'y a aucune erreur et s'il y a une erreur nous affichons un message de formulaire invalide.

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
+require_once 'class/Message.php';
+$errors = null;
+if (isset($_POST['username'], $_POST['message'])) {
+    $message = new Message($_POST['username'], $_POST['message']);
+    if ($message->isValid()) {
+        # code...
+    } else {
+        $errors = 'Formulaire invalide';
+    }
+    
+}
 $title = "Livre d'or";
 require 'elements/header.php';
 ?>
 
 <div class="container">
     <h1>Livre d'or</h1>
+
+    <?php if($errors): ?>
+        <div class="alert alert-danger">
+            <?= $errors ?>
+        </div>
+    <?php endif ?>
+
     <form action="" method="post">
         <div class="form-group">
             <input type="text" name="username" id="" placeholder="Votre pseudo" class="form-control">
