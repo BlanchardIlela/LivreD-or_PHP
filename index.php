d8ec08c48832c7a9b3f61bb94bbf8b9767fe4629
BlanchardIlela <blancilela@gmail.com>
2026-02-19 10:57:23 +0100

    Ajout de value pour afficher le nom de l'utilisateur, Instensiation de class GuestBokk et la méthode addMessage pour ajout de message

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'class/Message.php';
+require_once 'class/GuestBook';
 $errors = null;
 if (isset($_POST['username'], $_POST['message'])) {
     $message = new Message($_POST['username'], $_POST['message']);
     if ($message->isValid()) {
-        # code...
+        $guestbook = new GuestBook(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'messages');
+        $guestbook->addMessage($message);
     } else {
         $errors = $message->getErrors();
     }
@@ -25,13 +27,13 @@ require 'elements/header.php';
 
     <form action="" method="post">
         <div class="form-group">
-            <input type="text" name="username" id="" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?>">
+            <input value="<?= htmlentities($_POST['username'] ?? '') ?>" type="text" name="username" id="" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?>">
             <?php if(isset($errors['username'])): ?>
                 <div class="invalid-feedback"><?= $errors['username'] ?></div>
             <?php endif ?>
         </div>
         <div class="form-group">
-            <textarea name="message" id="" placeholder="Votre Message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?>"></textarea>
+            <textarea name="message" id="" placeholder="Votre Message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?>"><?= htmlentities($_POST['message'] ?? '') ?></textarea>
             <?php if(isset($errors['message'])): ?>
                 <div class="invalid-feedback"><?= $errors['message'] ?></div>
             <?php endif ?>
