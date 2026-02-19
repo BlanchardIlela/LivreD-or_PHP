8ed095f5f5f37f6e97f08afe8d424815e5df98b7
BlanchardIlela <blancilela@gmail.com>
2026-02-19 11:07:06 +0100

    Dans le constructeur nous vérifions si dossier existe sinon on crée avec la fonction mkdir en octroyant les permissions ainsi que le true à la fin, vérifions aussi si le fichier existe et sinon on crée cela avec la fonction touch.

--- class/GuestBook.php
+++ class/GuestBook.php
@@ -3,9 +3,15 @@ require_once 'Message.php';
 
 class GuestBook {
 
-    public function __construct(string $fichier)
+    public function __construct(string $file)
     {
-       
+       $directory = dirname($file);
+       if (!is_dir($directory)) {
+            mkdir($directory, 0777, true);
+       }
+       if (!file_exists($file)) {
+            touch($file);
+       }
     }
 
     public function addMessage(Message $message)
