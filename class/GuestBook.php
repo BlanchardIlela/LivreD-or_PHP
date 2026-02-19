cab8d0d4aeed44141feddf3fe196b3afdee4a5d3
BlanchardIlela <blancilela@gmail.com>
2026-02-19 11:15:21 +0100

    j'écris dans le fichier maintenat

--- class/GuestBook.php
+++ class/GuestBook.php
@@ -3,6 +3,8 @@ require_once 'Message.php';
 
 class GuestBook {
 
+    private $file;
+
     public function __construct(string $file)
     {
        $directory = dirname($file);
@@ -12,11 +14,12 @@ class GuestBook {
        if (!file_exists($file)) {
             touch($file);
        }
+       $this->file = $file;
     }
 
-    public function addMessage(Message $message)
+    public function addMessage(Message $message): void
     {
-        
+        file_put_contents($this->file, $message, FILE_APPEND);
     }
 
 }
