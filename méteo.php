912e3cb3df8fbc654b899ce91aaa85b407756323
BlanchardIlela <blancilela@gmail.com>
2026-02-21 20:42:00 +0100

    J'affiche la temperature du jour

--- méteo.php
+++ méteo.php
@@ -2,11 +2,13 @@
 require_once 'class/OpenWeather.php';
 $weather = new OpenWeather('825f6d28048deaead717e57bd7113032');
 $forecast = $weather->getForecast('Kinshasa,drc');
+$today = $weather->getToday('Kinshasa,drc');
 require 'elements/header.php';
 ?>
 
 <div class="container">
     <ul>
+        <li>En ce moment <?= $today['description']?> <?= $today['temp'] ?>°C</li>
         <?php foreach($forecast as $day): ?>
             <li><?= $day['date']->format('d/m/Y')?> <?= $day['description']?> <?= $day['temp'] ?>°C</li>
         <?php endforeach ?>
