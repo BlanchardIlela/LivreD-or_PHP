acdd4d1876a29870b184ee6d55b3ce15cb27c294
BlanchardIlela <blancilela@gmail.com>
2026-02-21 20:24:32 +0100

    Affichage de résultat à l'écran

--- méteo.php
+++ méteo.php
@@ -1,17 +1,16 @@
 <?php
-$curl = curl_init('https://api.openweathermap.org/data/2.5/weather?q=Kinshasa&units=metric&appid=825f6d28048deaead717e57bd7113032'); 
-curl_setopt_array($curl, [
-    CURLOPT_CAINFO => __DIR__ . DIRECTORY_SEPARATOR . 'Certificat.pem',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_TIMEOUT => 5
-]); 
-$data = curl_exec($curl);
-if ($data === false) {
-    var_dump(curl_error($curl));
-}else {
-    if(curl_getinfo($curl, CURLINFO_HTTP_CODE) === 200) {
-        $data = json_decode($data, true);
-        echo 'Il fait ' . $data['main']['temp'] . '°C';
-    }
-}
-curl_close($curl);
+require_once 'class/OpenWeather.php';
+$weather = new OpenWeather('825f6d28048deaead717e57bd7113032');
+$forecast = $weather->getForecast('Kinshasa,drc');
+require 'elements/header.php';
+?>
+
+<div class="container">
+    <ul>
+        <?php foreach($forecast as $day): ?>
+            <li><?= $day['date']->format('d/m/Y')?> <?= $day['description']?> <?= $day['temp'] ?>°C</li>
+        <?php endforeach ?>
+    </ul>
+</div>
+
+<?php require 'elements/footer.php';?>
